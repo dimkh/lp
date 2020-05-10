3efe9e31725b35940853623fd2fa0ca7d6772965
dimkh <dhaldeev@gmail.com>
2020-05-11 01:02:28 +0500

    ДЗ-2 выполнено не полностью

---
+++ src/scripts/rew.js
@@ -0,0 +1,104 @@
+import Vue from "vue";
+
+const rewSlide = {
+  template: "#rew-slide",
+  data() {
+    return {
+      dataUrl: "./content/images/"
+    };
+  },
+  props: {
+    slideInfo: Object
+  }
+};
+
+const rewSlider = {
+  template: "#rew-slider",
+  components: {
+    rewSlide
+  },
+  props: {
+    slidesInfo: Array
+  }
+};
+
+const btnsComp = {
+  template: "#rew-btns"
+};
+
+new Vue({
+  el: "talks-block",
+  template: "#rew-main",
+  components: {
+      btnsComp,
+      rewSlider
+  },
+  data() {
+    return {
+      slidesInfo: [],
+      currentSlides: [],
+      slidesIndex: 0
+    };
+  },
+
+  created() {
+    fetch("./data/rew.json")
+      .then(resp => resp.json())
+      .then(resp => {
+        this.slidesInfo = resp;
+      });
+  },
+
+  computed: {
+
+  },
+
+  methods: {
+    checkBtn(dir) {
+      let currentIndex = this.slidesIndex.Index;
+      let screenSlides = 0;
+      let sumSlidesWidth = 0;
+      let numberToMove = 0;
+
+      const defineSlider = this.$children[1].$refs.moveSlider;
+      const sliderBlock = this.$refs.sliderBlock;
+      let slidesArr = this.$children[1].$refs.moveSlider.children;
+
+      let sliderBlockWidth = parseInt(window.getComputedStyle(sliderBlock).width);
+
+      for (let slide of slidesArr) {
+        let slideWidth = parseInt(window.getComputedStyle(slide).width);
+        let marginWidth = parseInt(window.getComputedStyle(slide).marginRight);
+        let pleinWidth = slideWidth + marginWidth;
+        sumSlidesWidth += pleinWidth;
+      }
+
+      screenSlides = Math.ceil(sumSlidesWidth / sliderBlockWidth);
+
+      if (dir === "next") {
+        currentIndex++;
+        indexTest();
+        this.slidesIndex = currentIndex;
+      }
+      if (dir === "prev") {
+        currentIndex--;
+        indexTest();
+        this.slidesIndex = currentIndex;
+      }
+
+      function indexTest() {
+        if (currentIndex >= screenSlides) {
+          currentIndex = 0;
+        }
+        if (currentIndex < 0) {
+          currentIndex = 0;
+        }
+
+        numberToMove = 100 * currentIndex + '%';
+
+        defineSlider.style.transform = `translateX(-${numberToMove})`;
+      }
+    }
+  },
+});
+
